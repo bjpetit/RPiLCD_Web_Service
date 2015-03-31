5d3e7ded94abae3c8a053f8e63435cd3a2079488
Brent J. Petit <bjpetit@gmail.com>
2015-03-31 00:13:04 -0500

    Update the client example with digi

--- client/direwolf_parser.py
+++ client/direwolf_parser.py
@@ -1,4 +1,5 @@
 #
+# Quick and dirty script to 
 # Drive LCD web service from direwolf program
 #  Brent Petit - 2015
 #
@@ -13,8 +14,7 @@ from requests.auth import HTTPBasicAuth
 my_hostname="http://127.0.0.1:5000"
 my_username="admin"
 my_password="password"
-
-type_list = [ "receive", "transmit", "digi" ]
+my_call="N0CALL-1"
 
 # Execute direwolf and read stdin
 
@@ -23,7 +23,12 @@ popen = subprocess.Popen(args, stdout=subprocess.PIPE)
 for output in iter(popen.stdout.readline, ''):
    # Had some issues with exceptions due to unrecognized characters
    # not sure yet if this did the trick...
-   output = output.encode('utf-8', errors='replace')
+   try:
+      output = output.encode('utf-8', errors='replace')
+   except:
+      print "ERROR: Choked on input - ", sys.exc_info()[0]
+      continue
+
    print output,
 
    mygroup = re.match("(\[.+\])\s+([A-Za-z0-9\-]+)\>(.+)\:(.+)", output)
@@ -33,17 +38,28 @@ for output in iter(popen.stdout.readline, ''):
       #print "#3: " + mygroup.group(3)
       #print "#4: " + mygroup.group(4)
 
+      call = mygroup.group(2)
+      path = mygroup.group(3)
+
       if mygroup.group(1) == "[ig]":
-         type = "gate"
+         continue
       elif len(mygroup.group(1)) == 4:
          type = "transmit"
+         if re.search("TCPIP", output):
+            mygroup2 = re.match("[A-Za-z0-9\-\,]+\:\}([A-Za-z0-9\-\,]+)\>(.+)", 
+                                str(mygroup.group(3)))
+            if mygroup2:
+               call = mygroup2.group(1)
+               path = mygroup2.group(2)
+            else:
+               print "Error parsing iGate path: ", mygroup.group(3)
+            type = "igate"
+         elif re.search(my_call + "\*", output):
+            type ="digi"
       else:
          type = "receive"
 
       print "Type = " + type
-
-      call = mygroup.group(2)
-      path = mygroup.group(3)
 
       description = mygroup.group(4)
 
@@ -51,7 +67,7 @@ for output in iter(popen.stdout.readline, ''):
       headers = {'Content-Type': 'application/json'}
       payload = {'type': type, 'call': call, 'path': path, 'description': description}
       try:
-         r = requests.post(my_hostname+url, headers=headers, data=json.dumps(payload), 
+         r = requests.post(my_hostname+url, headers=headers, data=json.dumps(payload),
                            auth=HTTPBasicAuth(my_username, my_password))
       except:
          print "Unexpected Exception: ", sys.exc_info()[0]
